13efa23431b0b471d70918fee05709b62aa20291
erikcoin <erikdijkstra@hotmail.com>
2026-06-03 12:53:31 +0200

    Update bootpick.c

--- lvgl_ui_recovered/src/bootpick.c
+++ lvgl_ui_recovered/src/bootpick.c
@@ -19,8 +19,8 @@
 #include <string.h>
 #include <unistd.h>
 
-#define BP_HOR DISP_HOR
-#define BP_VER DISP_VER
+#define BP_HOR 800
+#define BP_VER 480
 #define BP_DRAW_LINES 100
 #define BP_COUNTDOWN_S 10
 
